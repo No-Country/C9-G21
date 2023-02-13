53ae1a7119c23cab01b4670a57959cfda0a5e6a6
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-02-13 16:50:51 -0500

    rutas_pedir_confirmar_buscar

--- BackEnd/routes/turnoRoutes.js
+++ BackEnd/routes/turnoRoutes.js
@@ -1,23 +1,23 @@
 import express from "express";
 import {
     crearTurno,
-    buscarTurno,
-    buscarTurnos,
-    actualizarTurno,
-    eliminarTurno
+    confirmarTurno,
+    buscarTurnos
 } from "../controllers/turnoController.js"
 
 const router = express.Router();
 
-/*Create Turno*/
+/*Crear Turno*/
 router.post("/pedir", crearTurno);
+/* Confirmar Turno */
+router.get("/confirmar/:Token", confirmarTurno);
 /*Buscar turno*/
 router.get("/buscar", buscarTurnos);
-/*Unico turno*/
-router.get("/turno/:id", buscarTurno);
-/*Actualizar turno*/
-router.put("/actualizarturno/:id", actualizarTurno);
-// Eliminar turno
-router.delete("/cancelacion/:id", eliminarTurno);
+// /*Unico turno*/
+// router.get("/turno/:id", buscarTurno);
+// /*Actualizar turno*/
+// router.put("/actualizarturno/:id", actualizarTurno);
+// // Eliminar turno
+// router.delete("/cancelacion/:id", eliminarTurno);
 
 export default router;
