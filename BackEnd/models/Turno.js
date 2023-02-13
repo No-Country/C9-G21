d89dca3b3d2d3bb32523f5a6d518a7b47fe61c45
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-02-13 16:49:12 -0500

    add_token

--- BackEnd/models/Turno.js
+++ BackEnd/models/Turno.js
@@ -1,40 +1,45 @@
 import mongoose from "mongoose";
+import generarId from "../helpers/generarId.js";
 
 
 const userSchema = mongoose.Schema({
-    Nombre:{
-        type: String,
-        required: true
-    },
-    Apellido:{
-        type: String,
-        required: true
-    },
-    Email:{
-        type: String,
-        required:true
-    },
-    Fecha:{
-        type: String,
-        required:true
-    },
-    Hora:{
-        type:String,
-        required:true
-    },
-    Servicio:{
-        type:String,
-        required:true
-    }, 
-    Confirmacion: {
-        type: Boolean,
-        default: false,
-      },
-    Disponible:{
-        type: Boolean,
-        default:true
-    }
+  Nombre: {
+    type: String,
+    required: true,
+  },
+  Apellido: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  Fecha: {
+    type: String,
+    required: true,
+  },
+  Hora: {
+    type: String,
+    required: true,
+  },
+  Servicio: {
+    type: String,
+    required: true,
+  },
+  Confirmacion: {
+    type: Boolean,
+    default: false,
+  },
+  Disponible: {
+    type: Boolean,
+    default: true,
+  },
+  Token:{
+    type: String,
+    default: generarId(),
+  }
 });
 
-const Turno= mongoose.model('Turno',userSchema);
-export default Turno
+const Turno = mongoose.model("Turno", userSchema);
+export default Turno;
