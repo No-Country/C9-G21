e422cdbe9018e0508b4046b263a3f9e417a37cd3
Cristhian Paez <72753117+Crissxp@users.noreply.github.com>
2023-02-14 20:56:56 -0300

    Update Navbar.tsx

--- FrontEnd/src/components/Navbar/Navbar.tsx
+++ FrontEnd/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode, useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Modal from "../Modal/Modal";
 import {
   Button,
   Navbar,
