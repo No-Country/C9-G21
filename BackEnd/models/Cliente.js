5c27a4f5b8d2558f8ea84897bb935a3207610fbe
aguusoleti <agustin_soleti@hotmail.com>
2023-02-16 22:13:59 -0300

    Borrado de direccion en clientes

--- BackEnd/models/Cliente.js
+++ BackEnd/models/Cliente.js
@@ -14,11 +14,6 @@ const clienteSchema = mongoose.Schema({
     required: true,
     trim: true,
   },
-  direccion: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   password: {
     type: String,
     required: true,
