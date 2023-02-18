af52189f56aa5a886f250b234eebf4faff66e569
aguusoleti <agustin_soleti@hotmail.com>
2023-02-18 17:48:24 -0300

    Validacion  de negocio Completa

--- BackEnd/middleware/validator.handler.js
+++ BackEnd/middleware/validator.handler.js
@@ -1,29 +1,42 @@
-import  Boom from '@hapi/boom';
-import { emailRegex } from '../helpers/validaciones.js';
+import {
+  validarTelefonoAr,
+  validarTelefonoPe,
+  validarTelefonoCl,
+  validarTelefonoCo,
+  validarTelefonoVe,
+  emailRegex,
+} from "../helpers/validaciones.js";
 
 function validatorHandler(schema, property) {
   return (req, res, next) => {
-  
     const data = req[property];
     const { error } = schema.validate(data, { abortEarly: false });
-    const errores=[];
+    const errores = [];
 
-    if ( data.email && !emailRegex.test(data.email) ) {
-      const error = new Error("Email incorrecto");
+    if ((
+      !validarTelefonoAr.test(data.telefono) ||
+      !validarTelefonoPe.test(data.telefono) ||
+      !validarTelefonoCl.test(data.telefono) ||
+      !validarTelefonoCo.test(data.telefono) ||
+      !validarTelefonoVe.test(data.telefono)
+    ) && data.telefono) {
+      const error = new Error("Formato de telefono no valido");
       return res.status(400).json({ msg: error.message });
     }
-    if (error) {
+      
+      if (data.email && !emailRegex.test(data.email)) {
+        const error = new Error("Email incorrecto");
+        return res.status(400).json({ msg: error.message });
+      }
+      if (error) {
+        error.details.forEach((element) => {
+          errores.push(element.context.key);
+        });
+        next(res.status(400).json(errores));
+      }
+      next();
+    
+  }};
 
 
-      (error.details.forEach(element => {
-        errores.push(element.context.key)
-      }))
-      next(
-        res.status(400).json(errores)
-      )
-    }
-    next();
-  }
-}
-
-export default validatorHandler
+export default validatorHandler;
