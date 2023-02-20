d433e2b524e594fe4afd4431138d11996218e25a
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2023-02-20 10:14:41 -0300

    page ok

--- FrontEnd/src/components/Error404/Error404.tsx
+++ FrontEnd/src/components/Error404/Error404.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Card, Container, Text, Col, Row, Button } from '@nextui-org/react';
+
 import { CSSBUTTONNEXT } from "@/const/constantsUI";
 
 const Error404 = () => {
 
-
+  
   return (
     <Container>
         <Card >
-                <Card.Body css={{ alignContent: "center" }}>
+                <Card.Body css={{ alignContent: "center", width:"100%", height:"100vh" }}>
                     <Col css={{
                         display: "flex",
                         alignItems: "center", flexDirection: "column"
                     }}>
-                        <Text id="modal-title" size={18} >
+                        <Text css={{fontFamily: "Comfortaa", color:"#078E85"}} size={24} >
                             Página no encontrada
                         </Text>
                         <Row justify="center">
@@ -21,8 +22,8 @@ const Error404 = () => {
                                 La página solicitada no existe. Haz click en el siguiente botón para ir a nuestra página de inicio.
                             </Text>
                         </Row>
-                        <Row justify='center'>
-                            <Button href="/index" css={CSSBUTTONNEXT}>INICIO</Button>
+                        <Row justify='center' css={{margin: "15px"}}>
+                            <Button  css={CSSBUTTONNEXT}>INICIO</Button>
                         </Row>
                         <Row>
                             <Card.Image src='error404.svg'></Card.Image>
