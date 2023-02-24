8dc0758958064a3ccac5c6a4e0b50298e48c8852
Cristhian Paez <72753117+Crissxp@users.noreply.github.com>
2023-02-24 19:50:12 -0300

    Update EditComerceCard.tsx

--- FrontEnd/src/components/register/EditComerceCard.tsx
+++ FrontEnd/src/components/register/EditComerceCard.tsx
@@ -1,6 +1,14 @@
 import { CSSBUTTONBACK, CSSBUTTONNEXT, INPUTPROPS } from "@/const/constantsUI";
-import { useState } from "react";
+import {
+  useState,
+  useContext,
+  useEffect,
+  ChangeEventHandler,
+  ChangeEvent,
+  FormEvent,
+} from "react";
 import { useRouter } from "next/router";
+import { globalContext } from "@/context/global.context";
 import {
   Container,
   Card,
@@ -14,91 +22,125 @@ import {
   Dropdown,
 } from "@nextui-org/react";
 
-import { string } from "zod";
-
 interface MyFormProps {
   initialValues: {
-    name: "string";
+    stepOne: {
+      name: string;
+      address: string;
+      city: string;
+      registeredName: string;
+    };
   };
 }
 
-export default function EditComerceCard() {
+const EditComerceCard: React.FC<MyFormProps> = ({ initialValues }) => {
   const [step, setstep] = useState(1);
   const [selected, setSelected] = useState<any>(new Set(["Rubro"]));
-  const [data, setdata] = useState({
-    nombreDelComercio: string,
-    razonSocial: string,
-    direccion: string,
-    ciudad: string,
-    rubro: string,
-    descripcion: string,
-    fotos: string,
-    horariosDeAtencion: string,
-  });
-
+  const [values, setValues] = useState(initialValues);
+  const { user } = useContext(globalContext);
   const router = useRouter();
 
   const handleRubro = (e: any) => {
     setSelected(e.target.value);
   };
 
-  const hadlerchange = (e: any) => {};
+  useEffect(() => {
+    if (!user.token) {
+      router.push("/");
+    }
+  }, [router, user.token]);
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+
+    setValues({ ...values, [name]: value });
+    console.log("hola");
+  };
 
   return (
-    <Container css={{ width: "100%", height: "100vh" }}>
+    <Container css={{ width: "100%", height: "100vh", marginTop: "3rem" }}>
       {step === 1 ? (
-        <Card aria-labelledby="Card-title">
-          <Card.Header>
-            <Text b size={18}>
-              Configura tu perfil
-            </Text>
-          </Card.Header>
-          <Card.Body>
-            <Col
-              css={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            ></Col>
-            <Input
-              {...INPUTPROPS}
-              placeholder="Las flores"
-              label="Nombre del comercio"
-              aria-label="Nombre del comercio"
-            />
-            <Input
-              {...INPUTPROPS}
-              placeholder="Peluquería Las Flores S. L."
-              label="Razón Social"
-              aria-label="Nombre del comercio"
-            />
-            <Input
-              {...INPUTPROPS}
-              placeholder="Mar del Plata"
-              label="Dirección"
-              aria-label="Nombre del comercio"
-            />
-            <Input
-              {...INPUTPROPS}
-              placeholder="Mar del Plata"
-              label="Ciudad"
-              aria-label="Nombre del comercio"
-            />
-          </Card.Body>
-          <Card.Footer>
-            <Row justify="center" css={{ paddingtop: "20px" }}>
-              <Button
-                auto
-                rounded
-                css={CSSBUTTONNEXT}
-                onPress={() => setstep(2)}
-              >
-                Continuar
-              </Button>
-            </Row>
-          </Card.Footer>
-        </Card>
+        <form>
+          <Card aria-labelledby="Card-title">
+            <Card.Header css={{ justifyContent: "center" }}>
+              <Text b size={18}>
+                Configura tu perfil
+              </Text>
+            </Card.Header>
+            <Card.Body>
+              <Col
+                css={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              ></Col>
+              <Input
+                {...INPUTPROPS}
+                required={true}
+                placeholder="Las flores"
+                label="Nombre del comercio"
+                aria-label="Nombre del comercio"
+                initialValue={user.data?.name}
+                value={values?.stepOne?.name}
+                id="name"
+                name="name"
+                onChange={handleChange}
+              />
+              <Input
+                {...INPUTPROPS}
+                placeholder="Peluquería Las Flores S. L."
+                label="Razón Social"
+                aria-label="Razón Social"
+                initialValue={user.data?.registeredName}
+                value={values?.stepOne?.registeredName}
+                name="registeredName"
+                onChange={handleChange}
+              />
+              <Input
+                {...INPUTPROPS}
+                placeholder="Mar del Plata"
+                label="Dirección"
+                aria-label="Nombre del comercio"
+                initialValue={user.data?.address}
+                value={values?.stepOne?.address}
+                name="address"
+                onChange={handleChange}
+              />
+              <Input
+                {...INPUTPROPS}
+                placeholder="Mar del Plata"
+                label="Ciudad"
+                aria-label="Nombre del comercio"
+                initialValue={user.data?.city}
+                value={values?.stepOne?.city}
+                name="city"
+                onChange={handleChange}
+              />
+            </Card.Body>
+            <Card.Footer>
+              <Row justify="center" css={{ paddingtop: "20px" }}>
+                <Button
+                  auto
+                  rounded
+                  css={CSSBUTTONNEXT}
+                  onPress={() =>
+                    values.stepOne?.name &&
+                    values.stepOne?.address &&
+                    values.stepOne?.city &&
+                    values.stepOne?.registeredName
+                      ? setstep(2)
+                      : ""
+                  }
+                >
+                  Continuar
+                </Button>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </form>
       ) : step === 2 ? (
         <Card aria-labelledby="Card-title">
           <Card.Header>
@@ -132,6 +174,7 @@ export default function EditComerceCard() {
             </Dropdown>
             <Textarea
               {...INPUTPROPS}
+              required
               minRows={2}
               maxRows={2}
               placeholder="¿Algo que quieras contar sobre tu comercio?"
@@ -141,6 +184,7 @@ export default function EditComerceCard() {
             />
             <Textarea
               {...INPUTPROPS}
+              required
               minRows={2}
               maxRows={2}
               placeholder="Podes subir las fotos de tu comercio aquí. (Máximo 12)"
@@ -247,4 +291,6 @@ export default function EditComerceCard() {
       )}
     </Container>
   );
-}
+};
+
+export default EditComerceCard;
